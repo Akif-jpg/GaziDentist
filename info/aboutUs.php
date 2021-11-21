6ed3b7fc51a2edcce5e6dfcab24ea6ab7c18eac2
Akif-jpg <akifesadi193@gmail.com>
2021-11-22 00:16:26 +0300

    hakkımızda düzenlendi

--- info/aboutUs.php
+++ info/aboutUs.php
@@ -3,12 +3,17 @@
     include_once "../includes/navigation.php";
     include_once "../includes/db.php";
 ?>
-    <div class="container">
-        <div class="row">
-            <!-- Buraya siteyi anlattığım ve güncellemelerden bahsettiğim bir bölüm gelecek. -->
-            Hakkımızda
-        </div>
+<div class="container">
+    <div class="row">
+        <!-- Buraya siteyi anlattığım ve güncellemelerden bahsettiğim bir bölüm gelecek. -->
+        <center>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/mcUQeUBwfzs"
+                title="YouTube video player" frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen></iframe>
+        </center>
     </div>
+</div>
 
 <?php
     include_once "../includes/sidebar.php";
