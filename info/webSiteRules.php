9acb7bc0a70f3ab1f4a1b1a20e7d6bdb75eb276b
Oztas-jpg <muhoztas402@gmail.com>
2021-11-21 22:48:09 +0300

    mis gibi oldu

---
+++ info/webSiteRules.php
@@ -0,0 +1,46 @@
+<?php include "../includes/header.php"; ?>
+<?php include "../includes/db.php"; ?>
+<?php include "../includes/navigation.php"; ?>
+<div class="container">
+    <div class="roll">
+        <div class="col-md-10">
+        <center> <h2>Forum Kuralları</h2> </center>
+            
+            <div class="w3-content w3-padding-32">
+                <ol>
+                    <li class="w3-padding-bottom">Foruma üye olan herkes bu şartları kabul etmiş sayılır. </li>
+                    <li class="w3-padding-bottom">Forumda yazılan mesajlardan tamamen yazan forum üyesi sorumludur. Şahısların yazdıkları yazılardan dolayı forumumuz hiçbir sorumluluk kabul etmez.</li>
+                    <li class="w3-padding-bottom">T.C. yasalarını ya da uluslararası kanunları, anlaşmaları, tüzükleri çiğneyen mesajlar foruma gönderilemez.</li>
+                    <li class="w3-padding-bottom">Tehdit edici, küfürlü, örf ve adetlere karşı, taciz edici, nefret dolu ya da çok miktarlarda istenmeyen mesajlar göndermek; din, dil, ırk ayrımına yönelik iletiler göndermek ve materyaller kullanmak; forum yöneticilerini ve kullanıcılarını küçümser davranışlar sergilemek yasaktır.</li>
+                    <li class="w3-padding-bottom">Forum admin ve yöneticileri kurallara uymayan mesajları değiştirme ve silme hakkına sahiptir.</li>
+                    <li class="w3-padding-bottom">Forum kurallarının güncellenme veya yenilerinin eklenmesi hakkı forum yöneticilerince her zaman saklıdır ve değiştikten sonra geçerli hale gelir.</li>
+                    <li class="w3-padding-bottom">Her türlü telif hakkı içeren paylaşım yasaktır. (crack, warez, mp3 vs...)</li>
+                    <li class="w3-padding-bottom">Forumumuzda kanun, ahlak ve adaba aykırı yazılar yazmak, müstehcen resimler eklemek ya da link vermek kesinlikle yasaktır. Aksini yapanların derhal erişim yetkileri alınacaktır.</li>
+                    <li class="w3-padding-bottom">Forumumuz üzerinden alınan herhangi bilgi veya dosyanın doğuracağı problemlerden kullanıcının kendisi mesuldür. Forumumuz bir bilgi paylaşım platformudur ve buradan edinilen bilgi veya dosyaları kullanmak özgürlüğü tamamen kullanıcıya aittir.</li>
+                    <li class="w3-padding-bottom">Forum katılımcıları yeni bir konu başlığı açmadan önce o konunun forumda mevcut olup olmadığını kontrol etmeliler. Bunun için forumun arama özelliği kullanılabilir. Aynı isimli veya aynı tartışmaları içeren konular açılamaz.</li>
+                    <li class="w3-padding-bottom">Yöneticiler her yazılan mesajı göremeyebilirler. Yöneticilerden önce uygunsuz bir mesajla karşılaştığınızda lütfen o bölümden sorumlu yöneticilere bildiriniz.</li>
+                    <li class="w3-padding-bottom">Mesaj yazarken sadece büyük harf kullanmak bağırmak ve saygısızlık anlamına geleceği için cümlenizin tamamını kesinlikle büyük harf kullanarak yazmayınız. Konu başlıklarını konunun içeriğini ifade eden, özetleyen şekilde seçmeniz gerekmektedir. Konu ile alakalı olmayan başlıklar silinebilir. Buna yapılan itirazlar göz önüne alınmayacaktır.</li>
+                    <li class="w3-padding-bottom">Foruma yazılacak konular ilgili konu başlıkları altına yazılmalıdır. Buna özellikle dikkat etmenizi rica ediyoruz, aksi takdirde forumunuzun yönetim tarafından silinmesi söz konusu olacaktır.</li>
+                    <li class="w3-padding-bottom">Üye olurken ahlaksız isim seçmek, imzalara siyasi amaçlı yazılar yazmak, reklam yapmak yasaktır. Resim olarak kesinlikle müstehcen ya da siyasi resimler seçilmemeli ve kesinlikle bir siyasi partinin simgesi olan logolar kullanılmamalıdır. Aksi halde üyeliğiniz silinecektir!</li>
+                    <li class="w3-padding-bottom">Yöneticiler, yazılan yazının konusuyla ilgili olmayan bir bölümde yazıldığına karar verirlerse yazıyı uygun bölüme aktarır veya silerler. Bu nedenle itiraz türü yazıların da silinmesi veya cevaplandırılması yönetici inisiyatifindedir.</li>
+                    <li class="w3-padding-bottom">Forum yönetimi tarafından, aynı kişinin birden fazla rumuz veya isimle, farklı kişilermiş gibi aynı konuda görüş belirttiğinin tespit edilmesi durumunda, kişinin tüm mesajları silinir ve içerik girişi yasaklanır. Yine buna bağlı olarak aynı kişinin birden fazla e mail adresi ile başvurarak farklı isimler ile yazdığının tespit edilmesi durumunda aktif olan üyeliği derhal silinir.</li>
+                    <li class="w3-padding-bottom">Site ve forumun işleyiş ve kuralları konusunda katılımcıları bilgilendirmek adminlerin veya moderatörlerin görevi değildir. Katılımcılar bu kuralları okuyup uymak zorundadır.</li>
+                    <li class="w3-padding-bottom">Hiç bir forum hakkında veya kişi ve kurumları karalayıcı, yöneticilerini küçük düşürücü, aşağılayıcı kelimeler kullanılamaz. Aksine tutum izleyen üyelerin üyelikleri hiç bir ihtarda bulunmaksızın derhal silinecektir!</li>
+                    <li class="w3-padding-bottom">Başka bir üye için ahlaka ve mevzuata aykırı veya aşağılayıcı kelimeler kullanan, ya da forumun huzurunu kaçırmak isteyen kullanıcılar derhal forumumuzdan uzaklaştırılırlar.</li>
+                    <li class="w3-padding-bottom">Forum kurallarına uymayan katılımcıların forumda yazı yazması engellenecektir. Bu işlem ihtarda bulunulmaksızın da yapılabilir.</li>
+                    <li class="w3-padding-bottom">Siteye üye olan her üye, üyelik kurallarını tamamen okumuş ve kabul etmiş sayılacağından, sonradan bilmediği yönünde bir beyanat kabul edilemez.</li>
+                    <li class="w3-padding-bottom">Üyelik gerektiren başka forumlara davet mesajları ve reklamlar dahil kesinlikle yasaktır. Bu durum tespitinde lütfen site yöneticilerine bildiriniz. Ayrıca üye bu durumda süresiz ban edilecektir. (erişimi durdurulacaktır).</li>
+                    <li class="w3-padding-bottom">Kullanıcıların kişisel bilgileri ile ilgili seçimlerinden site yönetimi sorumlu değildir. Unutmayınız ki foruma yazılan her türlü bilgi kamuya açık bilgi haline gelecektir. Üyeliğinizin iptali, üye isminizin değiştirilmesi, iletilerinizin silinmesi gibi taleplerinizin  kabul edilip edilmemesi site yöneticilerinin inisiyatifindedir.</li>
+                    <li class="w3-padding-bottom">Bu sözleşme ile bizi (burada ya da başka bir yerde belirtilmiş olsun ya da olmasın) tarafınızdan gönderilen uygunsuz veya gayri yasal verilerin tarafımızdan silinmesi ya da değiştirilmesi konusunda, yetkili kılmaktasınız. Site içinde mevzuata aykırı iletilerden sorumlu olmadığımız halde, uygunsuz görülen ileti ve bilgileri sileceğimiz gibi, üyeliğin askıya alınması yoluna gideceğimizi kabul etmektesiniz.</li>
+                </ol>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<?php include "../includes/bottom_bar.php"?>
+    <?php include "../includes/sidebar.php"; ?>      
+    <hr>
+    <!-- Including Footer PHP -->
+    <?php include "../includes/footer.php"; ?>
